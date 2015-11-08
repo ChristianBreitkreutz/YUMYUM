c88fd3ee4ebb748d8f5ef46a99dfe64ccd5e2b7b
Christian Breitkreutz <cbreitkreutz@epages.com>
2015-11-08 23:54:49 +0100

    build init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-read -p "Press [Enter] key to start boostrap process..."
-yum update
-yum install git python-pip python-yaml python-jinja2 aptitude
 
-#common part
-yum install ansible
-cd ~
-mkdir Code
-cd Code
-git clone git@github.com:ChrBrAteaPages/YUMYUM.git
-cd YUMYUM
-echo "Then run: cd ~/Code/YUMYUM; ansible-playbook desktop-provisioning.yaml -i inventories/inventory.ini --diff --ask-sudo-pass"
+if [ -n "$1" ] ;  then
+    echo "add user '$1' in 'wheel' group ( allow sudo for $1)"
+    usermod -a -G wheel $1
+    echo "install ansible"
+    yum update
+    yum install epel-release.noarch
+    yum install ansible
+  else
+    echo "please set <username>, like:"
+    echo ". init.sh HansWurst"
+fi
