d171d90a07883a02b8fc76b5aab783a5f86fd6e7
Christian Breitkreutz <cbreitkreutz@epages.com>
2017-04-06 14:06:58 +0200

    bunch of refactorings

--- roles/bashrc/files/common.sh
+++ roles/bashrc/files/common.sh
@@ -12,29 +12,13 @@ alias l.='ls -d .* --color=auto'
 # cd
 alias cd..='cd ..'
 alias ..='cd ..'
-alias .2='cd ../..'
-alias .3='cd ../../..'
-alias ...='cd ../../..'
-alias .4='cd ../../../..'
-alias ....='cd ../../../..'
-alias .5='cd ../../../../..'
-alias .6='cd ../../../../../..'
-
-alias .r='cd /root'
-alias .h='cd /home'
-if [ -d "/home/$USER/Schreibtisch" ]; then
-	alias .d="cd /home/$USER/Schreibtisch"
-else
-	alias .d="cd /home/$USER/Desktop"
-fi
-
 
 # rests
 alias tailf='tail -f -n50'
-alias c='clear'
 alias ping='ping -c 3'
 alias pingg='ping -c 1 www.google.com'
 alias df='df -H'
 
+alias catssh='cat ~/.ssh/*.pub'
 
 EDITOR=vim
